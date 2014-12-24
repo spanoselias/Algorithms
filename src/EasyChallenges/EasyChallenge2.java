c007e261c192d89f09dfc0a6a38c683e6c42e149
Elias Spanos <spanoselias@outlook.com>
2014-12-24 20:23:08 +0200

    challengeSolved

--- src/EasyChallenges/EasyChallenge2.java
+++ src/EasyChallenges/EasyChallenge2.java
@@ -1,7 +1,25 @@
 package EasyChallenges;
 
-/**
- * Created by Elias on 12/24/2014.
- */
-public class EasyChallenge2 {
+import java.util.Scanner;
+
+class EasyChallenge2 {
+    int FirstFactorial(int num) {
+
+        if(num==1)
+            return 1;
+
+        int result=num*FirstFactorial(num-1);
+
+
+        return result;
+
+    }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner  s = new Scanner(System.in);
+        EasyChallenge2 c = new EasyChallenge2();
+      //  System.out.print(c.FirstFactorial(s.nextLine()));
+    }
+
 }

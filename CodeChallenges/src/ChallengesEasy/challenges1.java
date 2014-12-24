6198b62be3bde1d7ea41357c434ed317e866ced7
Elias Spanos <spanoselias@outlook.com>
2014-12-24 19:29:50 +0200

    Solved Challenge

--- CodeChallenges/src/ChallengesEasy/challenges1.java
+++ CodeChallenges/src/ChallengesEasy/challenges1.java
@@ -2,8 +2,33 @@ package ChallengesEasy;
 
 import java.util.*;
 
-public class challenges1 {
+
+//Challenge1: For this challenge you will be reversing a string.
+
+import java.util.*;
+import java.io.*;
+
+class Function {
+    String FirstReverse(String str) {
+
+        String invString="";
+        for(int i=str.length()-1; i>=0; i--)
+        {
+            invString=invString + String.valueOf(str.charAt(i));
+
+        }
+
+        str=invString;
+        return str;
+
+    }
+
     public static void main (String[] args) {
-        System.out.println("test1");
+        // keep this function call here
+        Scanner  s = new Scanner(System.in);
+        Function c = new Function();
+        System.out.print(c.FirstReverse(s.nextLine()));
     }
-    }
+
+}
+

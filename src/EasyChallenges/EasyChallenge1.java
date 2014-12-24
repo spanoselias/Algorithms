8c3b29ca573a9da2e4d8c07e7745a6f1a8abc10c
Elias Spanos <spanoselias@outlook.com>
2014-12-24 20:19:55 +0200

    config

--- src/EasyChallenges/EasyChallenge1.java
+++ src/EasyChallenges/EasyChallenge1.java
@@ -2,10 +2,8 @@ package EasyChallenges;
 import java.util.*;
 import java.io.*;
 
-class Function {
+class EasyChallenge1 {
     String FirstReverse(String str) {
-
-
 
         String invString="";
         for(int i=str.length()-1; i>=0; i--)
@@ -21,13 +19,12 @@ class Function {
 
         str=invString;
         return str;
-
     }
 
     public static void main (String[] args) {
         // keep this function call here
         Scanner  s = new Scanner(System.in);
-        Function c = new Function();
+        EasyChallenge1 c = new EasyChallenge1();
         System.out.print(c.FirstReverse(s.nextLine()));
     }
 

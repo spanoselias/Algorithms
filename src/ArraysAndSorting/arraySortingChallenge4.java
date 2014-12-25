574c675a84f1c785c5c58eb67483c3b605b9debe
Elias Spanos <spanoselias@outlook.com>
2014-12-25 14:12:18 +0200

    challengeSolved

--- src/ArraysAndSorting/arraySortingChallenge4.java
+++ src/ArraysAndSorting/arraySortingChallenge4.java
@@ -6,9 +6,28 @@ import java.util.*;
 public class arraySortingChallenge4 {
     public static void main(String [] args)
     {
-        int[] array =new int[3];
         Scanner keyboard=new Scanner(System.in);
-        System.out.println("Give the size of the array");
+        int[] noArray =new int[1000];
+
+        int n,v;
+
+        System.out.println("Give the number that you want to search: ");
+        v=keyboard.nextInt();
+
+        System.out.println("Give the size of the array: ");
+        n=keyboard.nextInt();
+
+        for (int i=0; i<n; i++)
+        {
+            System.out.println("Give a number at position " + i + ":");
+            noArray[i]=keyboard.nextInt();
+        }
+
+        for (int i=0; i<n; i++) {
+
+            System.out.print(" " + noArray[i]);
+        }
+
 
 
     }

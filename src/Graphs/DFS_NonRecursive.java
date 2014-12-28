c3bd2b0cfcb9ae90d56e9985908141df449554a5
Elias Spanos <spanoselias@outlook.com>
2014-12-28 17:51:35 +0200

    GraphAlgorithms

--- src/Graphs/DFS_NonRecursive.java
+++ src/Graphs/DFS_NonRecursive.java
@@ -3,10 +3,14 @@ package Graphs;
 /**
  * Created by Elias on 12/26/2014.
  */
+import java.util.*;
 public class DFS_NonRecursive {
 
-    public boolean[][] createGraph(boolean[][]graph)
+    public boolean[][] createGraph(boolean[][]graph,int N)
     {
+
+
+
 
 
     }//Function CreateGraph
@@ -15,7 +19,9 @@ public class DFS_NonRecursive {
     public static void main(String [] args){
 
     System.out.println("Dfs algorithm");
-
-
+        int N=0;
+        Scanner keyboard=new Scanner(System.in);
+        System.out.println("Give the dimension of tha array");
+        N=keyboard.nextInt();
     }
 }

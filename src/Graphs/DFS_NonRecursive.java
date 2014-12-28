a2efb1351a2af41b2db7a3b2226de9f29abfa167
Elias Spanos <spanoselias@outlook.com>
2014-12-28 18:01:11 +0200

    GraphAlgorithms

--- src/Graphs/DFS_NonRecursive.java
+++ src/Graphs/DFS_NonRecursive.java
@@ -6,12 +6,21 @@ package Graphs;
 import java.util.*;
 public class DFS_NonRecursive {
 
-    public boolean[][] createGraph(boolean[][]graph,int N)
+    public boolean[][] createGraph()
     {
+        Scanner keyboard=new Scanner(System.in);
+        int N=0;
+        System.out.println("Give the dimension of tha array");
+        N=keyboard.nextInt();
 
+        boolean[][] graph=new boolean[N][N];
 
-
-
+        for(int i=0; i< graph.length;i++ ){
+            for(int j=0; i<graph.length; j++) {
+                System.out.println("Give true(False if is not) there are connection between Vertex" + i + "And" + j);
+                graph[i][j]=keyboard.nextBoolean();
+                }//For J
+            }//For I
 
     }//Function CreateGraph
 
@@ -19,9 +28,6 @@ public class DFS_NonRecursive {
     public static void main(String [] args){
 
     System.out.println("Dfs algorithm");
-        int N=0;
-        Scanner keyboard=new Scanner(System.in);
-        System.out.println("Give the dimension of tha array");
-        N=keyboard.nextInt();
+
     }
 }

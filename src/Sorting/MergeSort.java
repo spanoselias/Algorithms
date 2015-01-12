e7bed7b1119a9b3c5cbdb886842f77e75adaf116
Elias Spanos <spanoselias@outlook.com>
2015-01-12 19:39:06 +0200

    InsertionSort

---
+++ src/Sorting/MergeSort.java
@@ -0,0 +1,13 @@
+package Sorting;
+
+/**
+ * Created by Elias on 1/12/2015.
+ */
+public class MergeSort {
+
+
+    public static void main(String [] args)
+    {
+        System.out.println("MergeSort");
+    }
+}

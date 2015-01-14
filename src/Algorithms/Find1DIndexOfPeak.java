9edb255a366bf188d96ceec00eca74b23e1ac865
Elias Spanos <spanoselias@outlook.com>
2015-01-14 23:42:06 +0200

    Find Peak Algorithm

--- src/Algorithms/Find1DIndexOfPeak.java
+++ src/Algorithms/Find1DIndexOfPeak.java
@@ -24,7 +24,6 @@ public class Find1DIndexOfPeak {
     }//FindPeak Method
 
     public static void main(String [] args){
-
         Scanner keyboard=new Scanner(System.in);
         Random randomGenerator = new Random();
 

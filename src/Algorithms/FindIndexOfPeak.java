6bb7fc2bc6678349e486a9e9a0bdc45eb1910aeb
Elias Spanos <spanoselias@outlook.com>
2015-01-14 16:20:51 +0200

    InsertionSort

--- src/Algorithms/FindIndexOfPeak.java
+++ src/Algorithms/FindIndexOfPeak.java
@@ -1,7 +1,46 @@
 package Algorithms;
 
+import java.util.Random;
+import java.util.Scanner;
+
+
 /**
- * Created by Elias on 1/14/2015.
+ * Created by Elias on 1/12/2015.
  */
 public class FindIndexOfPeak {
+    public static void Insertionsort(int[] Numbers)
+    {
+        int j=0,key=0;
+        for (int i=1; i<Numbers.length; i++)
+        {
+            key=Numbers[i];
+            j=i-1;
+
+            while(j >-1 && Numbers[j]>key)
+            {
+                Numbers[j+1]=Numbers[j];
+                j=j-1;
+            }//While Statment
+            Numbers[j+1]=key;
+        }//For Statment
+    }//InsertionSort Method
+
+    public static void main(String [] args){
+
+        Scanner keyboard=new Scanner(System.in);
+        Random randomGenerator = new Random();
+
+        int size = randomGenerator.nextInt(1000000);
+        int a[]=new int [size];
+
+        for(int j=0; j<a.length; j++) {
+            a[j]=randomGenerator.nextInt(10000);
+        }//For J
+        Insertionsort(a);
+
+        for(int j=0; j<a.length; j++) {
+            if(j % 23 ==0){System.out.println();}
+            System.out.print(a[j] + " ,");
+        }//For J
+    }
 }

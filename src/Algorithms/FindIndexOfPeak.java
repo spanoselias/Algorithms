ef026c92a6bf6ecfae2439688ea07da8f77e86c7
Elias Spanos <spanoselias@outlook.com>
2015-01-14 16:22:25 +0200

    InsertionSort

--- src/Algorithms/FindIndexOfPeak.java
+++ src/Algorithms/FindIndexOfPeak.java
@@ -8,25 +8,15 @@ import java.util.Scanner;
  * Created by Elias on 1/12/2015.
  */
 public class FindIndexOfPeak {
-    public static void FindPeak(int[] Numbers,int n)
-    {        int j=0,key=0;
-        for (int i=1; i<Numbers.length; i++)
-        {
-            key=Numbers[i];
-            j=i-1;
+    public static int FindPeak(int[] Numbers,int n)
+    {
 
-            while(j >-1 && Numbers[j]>key)
-            {
-                Numbers[j+1]=Numbers[j];
-                j=j-1;
-            }//While Statment
-            Numbers[j+1]=key;
-        }//For Statment
-    }//InsertionSort Method
+    }//FindPeak Method
 
     public static void main(String [] args){
 
         Scanner keyboard=new Scanner(System.in);
+
 
     }
 }

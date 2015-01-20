d34b8cfa17fdc99a0e3f354575a6f1e8c623815e
Elias Spanos <espano01@cs.ucy.ac.cy>
2015-01-20 18:45:08 +0000

    GraphImplementation

--- src/Algorithms/Graphs/Graph.java
+++ src/Algorithms/Graphs/Graph.java
@@ -3,33 +3,30 @@ package Algorithms.Graphs;
 /**
  * Created by Elias on 1/20/2015.
  */
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Graph {
 
-    private HashMap<String,LinkedList<Integer>> graph;
+    private HashMap<String,LinkedList<String>> graph;
 
     public Graph()
     {
-        graph=new HashMap<String,LinkedList<Integer>>();
+        graph=new HashMap<String,LinkedList<String>>();
     }//Constructor
 
     public void addVertex(String vertexIn)
     {
-        graph.put(vertexIn, new LinkedList<Integer>());
+        graph.put(vertexIn, new LinkedList<String>());
     }//Add Vertex
 
-    public void addNeighbor(int vertex1,int vertex2)
+    public void addNeighbor(String vertex1,String vertex2)
     {
         graph.get(vertex1).add(vertex2);
     }//Add neighbor method
 
 
-    public LinkedList<Integer> getNeighbors(int vertex)
+    public LinkedList<String> getNeighbors(String vertex)
     {
         return graph.get(vertex);
 
@@ -37,6 +34,17 @@ public class Graph {
 
     public static void main(String [] args)
     {
+
+        Graph myGraph=new Graph();
+        myGraph.addVertex("1");
+        myGraph.addVertex("2");
+
+        myGraph.addNeighbor("1","2");
+        myGraph.addNeighbor("1","3");
+
+        LinkedList<String> edge=myGraph.getNeighbors("1");
+
+        System.out.println(Arrays.toString(edge.toArray()));
 
 
     }//Main

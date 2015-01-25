a96f3c15fd749e51dafc223e6b292658ab284801
Elias Spanos <espano01@cs.ucy.ac.cy>
2015-01-25 20:07:06 +0200

    Bfs Graph

--- src/GraphAlgorithms/Graphs/dfsSearch.java
+++ src/GraphAlgorithms/Graphs/dfsSearch.java
@@ -26,13 +26,9 @@ public class dfsSearch {
         {
             if (!marked[edge])
             {
-
                 dfs(edge);
             }
-
         }
-
     }
-
 
 }
---
+++ src/GraphAlgorithms/Graphs/runGraph.java
@@ -0,0 +1,11 @@
+package GraphAlgorithms.Graphs;
+
+/**
+ * Created by Elias on 1/25/2015.
+ */
+public class runGraph {
+    public static void  main (String[] args){
+
+
+    }
+}

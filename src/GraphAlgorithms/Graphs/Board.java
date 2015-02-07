034df8952d0a21f3bdd9c29d6d1163ea10cf7fc9
Elias Spanos <spanoselias@outlook.com>
2015-02-07 20:42:47 +0200

    8-Puzzle

--- src/GraphAlgorithms/Graphs/Board.java
+++ src/GraphAlgorithms/Graphs/Board.java
@@ -3,6 +3,8 @@ package GraphAlgorithms.Graphs;
 /**
  * Created by Elias on 2/7/2015.
  */
+import java.lang.*;
+
 public class Board {
     int board[][];
 
@@ -14,7 +16,6 @@ public class Board {
  public int manhattan()
  {
      int totalManhattan=0;
-
      for(int i=0; i<board[0].length; i++)
      {
          for(int j=0; j<board[1].length; j++)
@@ -22,12 +23,14 @@ public class Board {
              int curValue=board[i][j];
              if(curValue!=0)
              {
+                 int cordX=(curValue-1)%4;
+                 int cordY=(curValue-1)/4;
 
-
-
+                 totalManhattan += Math.abs(i - cordY)+ Math.abs(j-cordX);
              }//If is not the space
          }//For j
      }//For i
+     return totalManhattan;
  }
 
 
